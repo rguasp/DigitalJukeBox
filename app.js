899be96269be368343aef51010526dcfdb8fb286
Rafael Guasp <ralfyguasp@gmail.com>
2018-08-06 23:50:31 -0400

    working tracklist

--- app.js
+++ app.js
@@ -1,83 +1,102 @@
+// On Page Load
 $(document).ready(function(){   
-    axios({
-      method: "GET",
-      url: `https://stg-resque.hakuapp.com/albums.json`
-     })
-    .then(albumsWeGetBackFromApi => {
-      console.log(albumsWeGetBackFromApi.data)
-
-    
+  function getTrackList(albumId){    
+          axios({
+            method:"GET",
+            url: `https://stg-resque.hakuapp.com/songs.json?album_id=${albumId}`
+          })
+              .then(trackListWeGetBackFromApi => {
+                $('#tracklist').empty();
+                for(i=0; i < trackListWeGetBackFromApi.data.length; i++) {
+                  // sort function for tracklist
+                  trackListWeGetBackFromApi.data.sort(function(a,b) {return a.song_order-b.song_order})
+                  // appending tracklist to html with jquery
+                  $('#tracklist').append(`
+                  <li class="list-group-item clearfix">
+                  <span class="badge" id="badge-custom">${trackListWeGetBackFromApi.data[i].song_duration}</span>
+                  <div class="col-sm-2 set-padding-0>
+                  <div class="col-sm-1 text-center>
+                  <span id="tnum">${trackListWeGetBackFromApi.data[i].song_order}</span>
+                  </div>
+                  <div class="text-center set-padding-0" id="star">
+                  <a href="#" data-placement="top" title data-toggle="tooltip" data-original-title="mark as favorite!">
+                  <i class"star glyphicon glyphicon-star-empty" aria-hidden="true" style="font-size: 24px; color: #DDDDDD;"></i>
+                  </a>
+                  </div>
+                  </div>
+                  <div class="col-sm-9 set-padding-0">
+                  <span id="songname"> ${trackListWeGetBackFromApi.data[i].song_name}</span>
+                  </div>
+                  </li>
+                  `)
+                  //tracklabel
+                  // <span id="songlabel">${trackListWeGetBackFromApi.data[i].song_label[0]}</span>
+                }
+              })
+  }
+  // axios call to access api for albums
+  axios({
+    method: "GET",
+    url: `https://stg-resque.hakuapp.com/albums.json`
+  })
+  // after axios call then following code executed
+  .then(albumsWeGetBackFromApi => {
+    // on slide event listener
+    $("#myCarousel").on('slide.bs.carousel', function(evt) {
+      console.log($(evt.relatedTarget).index());
+      // I get the index of the active slide
+      let i = $(evt.relatedTarget).index();
+      // I get the id of the album in the slide
+      let albumId = albumsWeGetBackFromApi.data[i].id;
+      getTrackList(albumId);
+        })
     for(i=0; i < albumsWeGetBackFromApi.data.length; i++) {
-      let albumNumber = i;
+      let slideNumber = i;
+      // if/else statement for auto active selection on pageload
       if(i === 0){
+        // appends albums to carousel with jquery
         $('.carousel-inner').append(`
-        <div class="active item" data-slide-number="${albumNumber}">
-        <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
-        <p>${albumsWeGetBackFromApi.data[i].name}</p>
-        <p>${albumsWeGetBackFromApi.data[i].artist_name}</p>
-        </div>
-         `)
+          <div class="active item" data-slide-number="${slideNumber}" style="font-family: 'Roboto', sans-serif;">
+            <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
+            <p>${albumsWeGetBackFromApi.data[i].name}</p>
+            <p>${albumsWeGetBackFromApi.data[i].artist_name}</p>
+          </div>
+        `)
+        getTrackList(albumsWeGetBackFromApi.data[i].id);
       }else{
        $('.carousel-inner').append(`
-        <div class="item" data-slide-number="${albumNumber}">
-        <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
-        <p>${albumsWeGetBackFromApi.data[i].name}</p>
-        <p>${albumsWeGetBackFromApi.data[i].artist_name}</p>
-        </div>
+          <div class="item" data-slide-number="${slideNumber}" style="font-family: 'Roboto', sans-serif;">
+            <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
+            <p>${albumsWeGetBackFromApi.data[i].name}</p>
+            <p>${albumsWeGetBackFromApi.data[i].artist_name}</p>
+          </div>
         `)
       }
-      $('#carousel-selector-' + albumNumber).append(`
-      <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
+      // appends albums to carousel selector
+      $('#carousel-selector-' + slideNumber).append(`
+        <img src="${albumsWeGetBackFromApi.data[i].cover_photo_url}" alt="AlbumCover">
       `)
     }
-    })
-    .catch(err => {
-      console.log(err);
-    })
-    for(i=1; i < 6; i++){
-      let albumId = i;
-    axios({
-      method:"GET",
-      url: `https://stg-resque.hakuapp.com/songs.json?album_id=${albumId}`
-    })
-    .then(trackListWeGetBackFromApi => {
-      console.log(trackListWeGetBackFromApi.data)
-      for(i=0; i < trackListWeGetBackFromApi.data.length; i++) {
-        trackListWeGetBackFromApi.data.sort(function(a,b) {return a.song_order-b.song_order})
-      $('.tracklist').append(`
-      <h3> ${trackListWeGetBackFromApi.data[i].song_name}</h3>
-      <h4>  ${trackListWeGetBackFromApi.data[i].song_order}</h4>
-      <h4> ${trackListWeGetBackFromApi.data[i].song_label}</h4>
-      <h4> ${trackListWeGetBackFromApi.data[i].song_duration}</h4>
-      `)
-      }
-    })
-    .catch(err => {
-        console.log(err);
-      })
-    }
+  })
+  // if there is an error it will be console logged
+  .catch(err => {
+    console.log(err);
+  })
 
-$('#myCarousel').carousel({
-      interval: false
-});
+  
+  // Handles carousel switching interval
+  $('#myCarousel').carousel({
+    interval: false
+  });
 
-$('#carousel-text').html($('#slide-content-0').html());
+  $('#carousel-text').html($('#slide-content-0').html());
 
-//Handles the carousel thumbnails
-$('[id^=carousel-selector-]').click( function(){
-  var id = this.id.substr(this.id.lastIndexOf("-") + 1);
-  var id = parseInt(id);
-  $('#myCarousel').carousel(id);
-});
-
-
-// When the carousel slides, auto update the text
-$('#myCarousel').on('slid.bs.carousel', function (e) {
-       var id = $('.item.active').data('slide-number');
-      $('#carousel-text').html($('#slide-content-'+id).html());
-});
-
+  //Handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click( function(){
+    var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+    var id = parseInt(id);
+    $('#myCarousel').carousel(id);
+  });
     
-    
-  }); // end document ready
+}); // end document ready
   
